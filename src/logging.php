708200b02c0ad03e2844e3db677f974cc1d7674e
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2021-06-13 17:09:20 +0100

    added timezone identifier to log

--- src/logging.php
+++ src/logging.php
@@ -12,7 +12,7 @@ function savelog($obj) {
 
     $logPrefix = date("Y-m-d");
     $logPrefix .= "T";
-    $logPrefix .= date("H:i:s");
+    $logPrefix .= date("H:i:se");
     $logPrefix .= " INFO ";
 
     if ( gettype($obj) == "string" ) {
