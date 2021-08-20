31282c9f71cbb8ba1176a3474b7d3232efdb225c
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2021-08-20 21:42:11 +0100

    making bash scripts reboot safe

--- runReplyToMessages.sh
+++ runReplyToMessages.sh
@@ -12,13 +12,15 @@ LOCKFILE="/tmp/slackbot_replytomessages"
 if [ -e "$LOCKFILE" ]; then
     read lastPID <"$LOCKFILE"
     [ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+    echo "$(date) Lock file ${LOCKFILE} is in place but application is not running, removing lockfile and continuing."
     lockfile-remove "$LOCKFILE"
 fi
 
-lockfile-create --use-pid -r 0 "$LOCKFILE"
+# hide the lockfile creation message
+lockfile-create --use-pid -r 0 "$LOCKFILE" >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo "Lock file ${LOCKFILE} is in place, exiting without action."
-    exit 1
+    #echo "Lock file ${LOCKFILE} is in place, exiting without action."
+    exit 0
 fi
 
 $PHP "$SCRIPT" --loop --quiet "$@"
