71a8a7249417eff0d2b5022e384e92324bb94180
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2021-08-20 14:53:38 +0100

    added test message for presemester

--- backoffice/dumpEvents.php
+++ backoffice/dumpEvents.php
@@ -2,11 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$collection = (new MongoDB\Client)->slackbot->events;
 
+$collection = (new MongoDB\Client)->slackbot->events;
 $result = $collection->find();
 $result->toArray();
 file_put_contents("../dbdump_events.json", json_encode($result));
 
 
-
+$collection = (new MongoDB\Client)->slackbot->conversation_state;
+$result = $collection->find();
+$result->toArray();
+file_put_contents("../dbdump_conversation_state.json", json_encode($result));
