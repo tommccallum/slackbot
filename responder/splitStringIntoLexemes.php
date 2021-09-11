fe1229bdcda8bbbebbc0bfc426be307a8e3fe7e9
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2021-09-11 11:31:20 +0100

    added fix for utf8 issue

--- responder/splitStringIntoLexemes.php
+++ responder/splitStringIntoLexemes.php
@@ -5,6 +5,11 @@ function splitStringIntoLexemes($message)
     // EMOJI are ::[A-Za-z0-9_]::
     // Learning outcomes
     // Slack users U\w{10}
+    
+    # fix bug where we tripped over unicode apostrophes not being recognised/scrambled
+    $message = json_encode($message);
+    $message = preg_replace("/\u2019/", "'", $message);
+    $message = json_decode($message, true);
 
     // TODO add in variables so we can use this with internal strings as well
     preg_match_all('/(::[A-Za-z0-9_]+::|:[A-Za-z0-9_]+:|[Ll][Oo]\s*\d\.\d\.\d\.\d|\d.\d.\d.\d|[Uu]\w{10}|\p{L}+\'t|[\p{L}\-]+|\p{N}[\.\p{N}]+|\.\p{N}+|\p{P}+|\S)/', $message, $words);
